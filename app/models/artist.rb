6500117afad5530413a8d5fc5f85242357ce30d0
Alice Freda <alicefreda@gmail.com>
2019-12-31 00:44:27 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,9 @@ class Artist < ActiveRecord::Base
   has_many :songs
   has_many :genres, through: :songs
 
+  def get_first_song
+    self.songs.first
+  end
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
   end
